a4ad303b4e2b4fe6a6ced0750c1fda4daf01e9e6
fedediclaudio <federicodicla@gmail.com>
2022-09-30 09:24:42 -0300

    Estructura de Proyecto

---
+++ src/main/java/com/dbd/realstate/RealstateTestApplication.java
@@ -0,0 +1,13 @@
+package com.dbd.realstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealstateTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealstateTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbd/realstate/RealstateTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbd.realstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealstateTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
